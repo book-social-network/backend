d365770a351fb3ce0da413baec734ddef5d37de6
Lê Minh Đức <leminhduc5732@gmail.com>
2024-11-14 15:21:04 +0700

    fix 404

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -10,61 +10,65 @@ use Illuminate\Http\Request;
 
 class FollowController extends Controller
 {
-    private $user,$follow, $notification;
-    public function __construct(UserInterface $userInterface,FollowInterface $followInterface, NotificationInterface $notificationInterface){
-        $this->follow=$followInterface;
-        $this->notification=$notificationInterface;
-        $this->user=$userInterface;
+    private $user, $follow, $notification;
+    public function __construct(UserInterface $userInterface, FollowInterface $followInterface, NotificationInterface $notificationInterface)
+    {
+        $this->follow = $followInterface;
+        $this->notification = $notificationInterface;
+        $this->user = $userInterface;
     }
-    public function getAllFollow(){
-        $user=auth()->user();
-        if(!$user){
+    public function getAllFollow()
+    {
+        $user = auth()->user();
+        if (!$user) {
             return response()->json(['message' => 'Please login'], 404);
         }
-        $followers=$this->follow->getAllFollowOfUser($user->id);
+        $followers = $this->follow->getAllFollowOfUser($user->id);
         return response()->json([
             'user' => $user,
             'followers' => $followers
         ]);
     }
-    public function handleFollow($id){
-        $user=auth()->user();
-        $follower=$this->user->getUser($id);
-        if(!$user || !$follower){
+    public function handleFollow($id)
+    {
+        $user = auth()->user();
+        $follower = $this->user->getUser($id);
+        if (!$user || !$follower) {
             return response()->json(['message' => 'Please login before following user'], 404);
         }
-        $follow=$this->follow->insertFollow([
+        $follow = $this->follow->insertFollow([
             'user_id' => $user->id,
             'follower' => $follower->id
         ]);
         // handle Realtime notification
 
         // follower
-        $notification=$this->notification->insertNotification([
+        $notification = $this->notification->insertNotification([
             'from_id' => $user->id,
             'to_id' => $follower->id,
-            'information' => $follower->name.' vừa gửi theo dõi bạn',
+            'information' => $follower->name . ' vừa gửi theo dõi bạn',
             'from_type' => 'user',
         ]);
         // broadcast(new NotificationSent($user,$notification->information));
         return response()->json(['message' => 'Follow successful']);
     }
-    public function handleUnfollow($id){
-        $user=auth()->user();
-        $follow=$this->follow->getFollow($id, $user->id);
-        if(!$user || !$follow){
+    public function handleUnfollow($id)
+    {
+        $user = auth()->user();
+        $follow = $this->follow->getFollow($id, $user->id);
+        if (!$user || !$follow) {
             return response()->json(['message' => 'Please login before following user'], 404);
         }
         $this->follow->deleteFollow($follow->id);
-        return response()->json(['message' => 'Unfollow successful'], 404);
+        return response()->json(['message' => 'Unfollow successful']);
     }
-    public function suggestFriends(){
-        $user=auth()->user();
-        if(!$user){
+    public function suggestFriends()
+    {
+        $user = auth()->user();
+        if (!$user) {
             return response()->json(['message' => 'Please login before following user'], 404);
         }
-        $friend=$this->follow->suggestFriends($user->id);
+        $friend = $this->follow->suggestFriends($user->id);
         return response()->json($friend);
     }
-
 }
