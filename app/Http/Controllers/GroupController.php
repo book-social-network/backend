04a508de8d5e7ac7b4a09156a0ff7357e4c9b1fe
Nguyễn Văn Hoàng Tâm <nguyenvanhoangtam2556@gmail.com>
2024-11-18 21:57:37 +0700

    update get all post in group

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -124,6 +124,7 @@ class GroupController extends Controller
         }
         return response()->json([
             'group' => $group,
+            'users' => $group->user()->get(),
             'posts' => $data
         ]);
     }
