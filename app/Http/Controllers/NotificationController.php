b1e4d7c34763c7d413fb3bfaae8fded4c32c412c
Lê Minh Đức <leminhduc5732@gmail.com>
2024-12-01 22:01:23 +0700

    no

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -8,26 +8,28 @@ use Illuminate\Http\Request;
 class NotificationController extends Controller
 {
     private $notification;
-    public function __construct(NotificationInterface $notificationInterface){
+    public function __construct(NotificationInterface $notificationInterface)
+    {
         $this->notification = $notificationInterface;
     }
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $page = $request->get('page', 1);
-        $user=auth()->user();
-        if(empty($user)){
-            return response()->json(['message' => 'Please login'],404);
+        $user = auth()->user();
+        if (empty($user)) {
+            return response()->json(['message' => 'Please login'], 404);
         }
-        $quantity=$this->notification->getQuantityNotification($user->id);
-        $notifications=$this->notification->getAllNotificationOfUser($user->id,$page,10 );
-        $quantityPages=$this->notification->getQuantityPageNotificationOfUser($user->id,10);
-        $data=[];
-        foreach($notifications as $notification){
-            $data[]=[
+        $quantity = $this->notification->getQuantityNotification($user->id);
+        $notifications = $this->notification->getAllNotificationOfUser($user->id, $page, 10);
+        $quantityPages = $this->notification->getQuantityPageNotificationOfUser($user->id, 10);
+        $data = [];
+        foreach ($notifications as $notification) {
+            $data[] = [
                 'id' => $notification->id,
-                'information' =>$notification->information,
+                'information' => $notification->information,
                 'state' => $notification->state,
-                'type_id' => $notification->from_id.'-'.$notification->to_id,
-                'type' => $notification->from_type.'-'.$notification->to_type,
+                'type_id' => $notification->from_id . '-' . $notification->to_id,
+                'type' => $notification->from_type . '-' . $notification->to_type,
             ];
         }
         return response()->json([
@@ -36,18 +38,20 @@ class NotificationController extends Controller
             'quantity' => $quantity
         ]);
     }
-    public function updateState($id){
-        $notification=$this->notification->getNotification($id);
-        if(empty($notification)){
-            return response()->json(['message' => 'Not found notification'],404);
+    public function updateState($id)
+    {
+        $notification = $this->notification->getNotification($id);
+        if (empty($notification)) {
+            return response()->json(['message' => 'Not found notification'], 404);
         }
-        $this->notification->updateNotification(['state'=>1],$notification->id);
+        $this->notification->updateNotification(['state' => 1], $notification->id);
         return response()->json(['message' => 'update notification successfully'],);
     }
-    public function delete($id){
-        $notification=$this->notification->getNotification($id);
-        if(empty($notification)){
-            return response()->json(['message' => 'Not found notification'],404);
+    public function delete($id)
+    {
+        $notification = $this->notification->getNotification($id);
+        if (empty($notification)) {
+            return response()->json(['message' => 'Not found notification'], 404);
         }
         $this->notification->deleteNotification($id);
         return response()->json(['message' => 'delete notification successfully'],);
